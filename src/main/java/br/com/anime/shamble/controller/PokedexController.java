f4a03d698e2019f7ccfb307091bd5d33fb23f8f1
FagnerRumenigg <rumeniggmoraes@gmail.com>
2024-01-23 21:57:22 -0300

    Criando crud básico da classe Pokedex e usando Optional

--- src/main/java/br/com/anime/shamble/controller/PokedexController.java
+++ src/main/java/br/com/anime/shamble/controller/PokedexController.java
@@ -1,13 +1,44 @@
 package br.com.anime.shamble.controller;
 
+import br.com.anime.shamble.dto.PokedexDto;
 import br.com.anime.shamble.model.Pokedex;
 import br.com.anime.shamble.repository.PokedexRepository;
+import br.com.anime.shamble.service.PokedexService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.nio.file.Path;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/pokedex")
 public class PokedexController {
+    @Autowired
+    PokedexService service;
+
+    @GetMapping
+    public List<PokedexDto> getall(){
+        return service.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public PokedexDto findById(@PathVariable int id){
+        return service.findById(id);
+    }
+    @PostMapping
+    public void save(@RequestBody PokedexDto dto){
+        service.savePokemon(dto);
+    }
+
+    @PutMapping("/{id}")
+    public void update(@PathVariable int id, @RequestBody PokedexDto dto){
+        service.updatePokemon(id, dto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable int id){
+        service.deletePokemon(id);
+    }
+
 }
--- src/main/java/br/com/anime/shamble/service/PokedexService.java
+++ src/main/java/br/com/anime/shamble/service/PokedexService.java
@@ -4,19 +4,79 @@ import br.com.anime.shamble.dto.PokedexDto;
 import br.com.anime.shamble.model.Pokedex;
 import br.com.anime.shamble.repository.PokedexRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class PokedexService {
 
     @Autowired
     PokedexRepository repository;
 
+    public List<PokedexDto> getAll(){
+        List<PokedexDto> listDto = new ArrayList<PokedexDto>();
+        List<Pokedex> listPokemon =  repository.findAll();
+
+        for(Pokedex pokemon : listPokemon){
+            listDto.add(fillDto(pokemon.getId(), pokemon.getName(), pokemon.getGen()));;
+        }
+        return listDto;
+    }
+    private Optional<Pokedex> getById(int id){
+        return repository.findById(id);
+    }
+
+    public PokedexDto findById(int id){
+        Optional<Pokedex> result = getById(id);
+
+        if(result.isPresent()){
+            Pokedex pokemon = result.get();
+            return fillDto(pokemon.getId(), pokemon.getName(), pokemon.getGen());
+        };
+        return null;
+    };
+
     public void savePokemon(PokedexDto pokemonDto){
         Pokedex pokemon = new Pokedex();
 
-        pokemon.setId(pokemonDto.getNational());
-        pokemon.setName(pokemonDto.getName());
-        pokemon.setGen(pokemonDto.getGen());
+        repository.save(fillPokedex(pokemon, pokemonDto.getNational(), pokemonDto.getName(), pokemonDto.getGen()));
+    }
 
-        repository.save(pokemon);
+    public void updatePokemon(int id, PokedexDto pokemonDto){
+        Optional<Pokedex> result  = getById(id);
+
+        if(result.isPresent()){
+            Pokedex pokemon = result.get();
+
+            repository.save(fillPokedex(pokemon, pokemonDto.getNational(), pokemonDto.getName(), pokemonDto.getGen()));
+        }
+    }
+
+    public void deletePokemon(int id){
+        repository.deleteById(id);
+    }
+
+    private PokedexDto fillDto(int id, String name, int gen){
+        PokedexDto pokemonDto = new PokedexDto();
+
+        pokemonDto.setNational(id);
+        pokemonDto.setName(name);
+        pokemonDto.setGen(gen);
+
+        return pokemonDto;
+    }
+
+    private Pokedex fillPokedex(Pokedex pokemon, int id, String name, int gen){
+        System.out.println(pokemon);
+
+        pokemon.setId(id);
+        pokemon.setName(name);
+        pokemon.setGen(gen);
+        System.out.println(pokemon);
+
+        return pokemon;
     }
 }
